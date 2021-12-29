93e617cb0c79ef825167284ded59f7ddf7debe17
fp024 <aopenasus@naver.com>
2021-12-30 05:25:50 +0900

    레시피 2-09-iv
    뭔가 의미가 있는 예제는 아닌 것 같다.

---
+++ chap02/recipe-2-09-iv/src/main/java/org/fp024/study/spring5recipes/sequence/Main.java
@@ -0,0 +1,21 @@
+package org.fp024.study.spring5recipes.sequence;
+
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+@Slf4j
+public class Main {
+  public static void main(String[] args) {
+    try (GenericXmlApplicationContext context =
+        new GenericXmlApplicationContext("appContext.xml")) {
+
+      LOGGER.info("beans: {}", Arrays.toString(context.getBeanDefinitionNames()));
+
+      SequenceGenerator generator = context.getBean("sequenceGenerator", SequenceGenerator.class);
+
+      LOGGER.info(generator.getSequence());
+      LOGGER.info(generator.getSequence());
+    }
+  }
+}
---
+++ chap02/recipe-2-09-iv/src/main/java/org/fp024/study/spring5recipes/sequence/SequenceGenerator.java
@@ -0,0 +1,31 @@
+package org.fp024.study.spring5recipes.sequence;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+public class SequenceGenerator {
+
+  @Setter private PrefixGenerator prefixGenerator;
+
+  @Setter private String suffix;
+
+  @Setter private int initial;
+
+  private final AtomicInteger counter = new AtomicInteger();
+
+  public SequenceGenerator(PrefixGenerator prefixGenerator, String suffix, int initial) {
+    this.prefixGenerator = prefixGenerator;
+    this.suffix = suffix;
+    this.initial = initial;
+  }
+
+  public String getSequence() {
+    StringBuilder builder = new StringBuilder();
+    builder.append(prefixGenerator.getPrefix());
+    builder.append(initial).append(counter.getAndIncrement());
+    builder.append(suffix);
+    return builder.toString();
+  }
+}
