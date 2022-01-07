73ac9ee2984c7b3533d8c37040d1b0fb61860538
fp024 <aopenasus@naver.com>
2022-01-08 06:18:33 +0900

    레시피 2-16-ii
    애스펙트 포인트컷 재사용하기
    * @Poincut을 관리하는 별도 클래스를 만들어서 사용한 예시
    * CalculatorPointcuts 클래스 참고

---
+++ chap02/recipe-2-16-ii/src/main/java/org/fp024/study/spring5recipes/calculator/CalculatorPointcuts.java
@@ -0,0 +1,10 @@
+package org.fp024.study.spring5recipes.calculator;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class CalculatorPointcuts {
+  @Pointcut("execution(* *.*(..))")
+  public void loggingOperation() {}
+}
