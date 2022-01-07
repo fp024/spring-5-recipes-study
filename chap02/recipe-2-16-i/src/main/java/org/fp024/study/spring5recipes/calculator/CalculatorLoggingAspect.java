fe08e0857b8aa5778ef2da2e57ddb5882aaaa6e5
fp024 <aopenasus@naver.com>
2022-01-08 06:07:43 +0900

    레시피 2-16-i
    애스펙트 포인트컷 재사용하기
    @Poincut 으로 포인트컷 메서드를 지정해서 재사용 방법을 보여주는 예시

---
+++ chap02/recipe-2-16-i/src/main/java/org/fp024/study/spring5recipes/calculator/CalculatorLoggingAspect.java
@@ -0,0 +1,76 @@
+package org.fp024.study.spring5recipes.calculator;
+
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class CalculatorLoggingAspect {
+
+  @Pointcut("execution(* *.*(..))")
+  private void loggingOperation() {}
+
+  // CalculatorLoggingAspect의
+  // loggingOperation()메서드에 설정된 포인트컷을 재사용하고 있다.
+  @Before("CalculatorLoggingAspect.loggingOperation()")
+  public void logBefore(JoinPoint joinPoint) {
+    LOGGER.info(
+        "The method "
+            + joinPoint.getSignature().getName()
+            + "() begins with "
+            + Arrays.toString(joinPoint.getArgs()));
+  }
+
+  @After("CalculatorLoggingAspect.loggingOperation()")
+  public void logAfter(JoinPoint joinPoint) {
+    LOGGER.info("The method " + joinPoint.getSignature().getName() + "() ends");
+  }
+
+  @AfterReturning(pointcut = "CalculatorLoggingAspect.loggingOperation()", returning = "result")
+  public void logAfterReturning(JoinPoint joinPoint, Object result) {
+    LOGGER.info("The method " + joinPoint.getSignature().getName() + "() ends with " + result);
+  }
+
+  @AfterThrowing(pointcut = "CalculatorLoggingAspect.loggingOperation()", throwing = "e")
+  public void logAfterThrowing(JoinPoint joinPoint, IllegalArgumentException e) {
+    LOGGER.error(
+        "Illegal argument "
+            + Arrays.toString(joinPoint.getArgs())
+            + " in "
+            + joinPoint.getSignature().getName()
+            + "()");
+  }
+
+  @Around("CalculatorLoggingAspect.loggingOperation()")
+  public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+    LOGGER.info(
+        "The method "
+            + joinPoint.getSignature().getName()
+            + "() begins with "
+            + Arrays.toString(joinPoint.getArgs()));
+    try {
+      Object result = joinPoint.proceed();
+      LOGGER.info("The method " + joinPoint.getSignature().getName() + "() ends with " + result);
+      return result;
+    } catch (IllegalArgumentException e) {
+      LOGGER.error(
+          "Illegal argument "
+              + Arrays.toString(joinPoint.getArgs())
+              + " in "
+              + joinPoint.getSignature().getName()
+              + "()");
+      throw e;
+    }
+  }
+}
