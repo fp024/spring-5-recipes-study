508c5b8f6e72fa708d8406f0261c753de7509efd
fp024 <aopenasus@naver.com>
2022-01-12 16:44:43 +0900

    spring-instrument 는 런타임에만 써서, 관련해서 변경해보았다.
    그래도 책의 결과대로 나오지는 않음.

--- chap02/recipe-2-20-iii/src/main/java/org/fp024/study/spring5recipes/calculator/CalculatorConfiguration.java
+++ chap02/recipe-2-20-iii/src/main/java/org/fp024/study/spring5recipes/calculator/CalculatorConfiguration.java
@@ -2,11 +2,10 @@ package org.fp024.study.spring5recipes.calculator;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
 @Configuration
-@EnableAspectJAutoProxy
+// @EnableAspectJAutoProxy
 @EnableLoadTimeWeaving
 @ComponentScan
 public class CalculatorConfiguration {}
