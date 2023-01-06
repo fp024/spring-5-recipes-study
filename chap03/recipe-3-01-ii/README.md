08a03547197b667648a13ef924d0458675763232
fp024 <aopenasus@naver.com>
2023-01-06 19:16:21 +0900

    레시피 3-1-ii 간단한 스프링 MVC 웹 애플리케이션 개발하기 2
    `AbstractAnnotationConfigDispatcherServletInitializer` 를 상속해서 서블릿 설정하는 예시

---
+++ chap03/recipe-3-01-ii/README.md
@@ -0,0 +1,12 @@
+## 레시피 3-1-ii 간단한 스프링 MVC 웹 애플리케이션 개발하기 2
+
+
+
+## 진행
+
+### 이전 기반 예제와 다른 점
+
+* `AbstractAnnotationConfigDispatcherServletInitializer` 를 상속해서 서블릿 설정
+  * META-INF의 javax.servlet.ServletContainerInitializer 파일도 삭제해야함.
+
+
---
+++ chap03/recipe-3-01-ii/src/test/java/org/fp024/study/spring5recipes/court/web/ReservationQueryControllerTest.java
@@ -0,0 +1,51 @@
+package org.fp024.study.spring5recipes.court.web;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.fp024.study.spring5recipes.court.config.CourtConfiguration;
+import org.fp024.study.spring5recipes.court.service.ReservationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@SpringJUnitWebConfig(classes = {CourtConfiguration.class})
+class ReservationQueryControllerTest {
+  private MockMvc mockMvc;
+
+  @Autowired private ReservationService reservationService;
+
+  @BeforeEach
+  void setUp() {
+    this.mockMvc =
+        MockMvcBuilders.standaloneSetup(new ReservationQueryController(reservationService)).build();
+  }
+
+  @Test
+  void testSetupForm() throws Exception {
+    mockMvc
+        .perform(get("/reservationQuery/")) //
+        .andExpect(status().isOk())
+        .andExpect(view().name("reservationQuery"));
+  }
+
+  @Test
+  void testSubmitForm() throws Exception {
+    mockMvc
+        .perform(
+            post("/reservationQuery/") //
+                .param("courtName", "Tennis #1"))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeExists("reservations"))
+        .andExpect(model().attribute("reservations", hasSize(1)))
+        .andExpect(view().name("reservationQuery"))
+        .andReturn();
+  }
+}
