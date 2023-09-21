07084f8c695ff8d9c590a3fe13862234389e6b4b
fp024 <aopenasus@naver.com>
2023-09-22 05:24:13 +0900

    4-01-iii 예제는 따로 안만들어도 되겠다.
    그냥 ii 예제에서 `@RestController`로만 바꾼 내용이다.
    * 그에 따라 @ResponseBody를 따로 입력하지 않아도 됨.

--- chap04/recipe-4-01-ii/src/main/java/org/fp024/study/spring5recipes/court/web/RestMemberController.java
+++ chap04/recipe-4-01-ii/src/main/java/org/fp024/study/spring5recipes/court/web/RestMemberController.java
@@ -3,18 +3,16 @@ package org.fp024.study.spring5recipes.court.web;
 import lombok.RequiredArgsConstructor;
 import org.fp024.study.spring5recipes.court.domain.Members;
 import org.fp024.study.spring5recipes.court.service.MemberService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class RestMemberController {
 
   private final MemberService memberService;
 
   @RequestMapping(value = "/members.xml")
-  @ResponseBody
   public Members getRestMembersXML() {
     Members members = new Members();
     members.addMembers(memberService.findAll());
