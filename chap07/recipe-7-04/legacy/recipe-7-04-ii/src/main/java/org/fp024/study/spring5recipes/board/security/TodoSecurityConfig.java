6c1a747ff8e096fe293dcfac0029f68b6bb149e7
fp024 <aopenasus@naver.com>
2023-11-09 12:06:39 +0900

    레시피 7-04-ii 접근 통제 결정하기  - 표현식을 이용해 접근 통제 결정 - `레거시`
    - 표현식 동작 확인
    - 표현식 클래스를 직접 만드는 것은 다음 커밋에 하자.

---
+++ chap07/recipe-7-04/legacy/recipe-7-04-ii/src/main/java/org/fp024/study/spring5recipes/board/security/TodoSecurityConfig.java
@@ -0,0 +1,53 @@
+package org.fp024.study.spring5recipes.board.security;
+
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class TodoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final DataSource dataSource;
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  JdbcUserDetailsManager jdbcUserDetailsManager() {
+    JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+    return jdbcUserDetailsManager;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers(HttpMethod.DELETE, "/todos/*")
+        .access(
+            "hasAuthority('ADMIN') and (hasIpAddress('127.0.0.1') or hasIpAddress('0:0:0:0:0:0:0:1'))")
+        //
+        .and()
+        .formLogin()
+        .loginPage("/login") //
+        .loginProcessingUrl("/login")
+        .defaultSuccessUrl("/todos")
+        .failureUrl("/login?error=true")
+        .and()
+        .rememberMe()
+        .userDetailsService(jdbcUserDetailsManager())
+        .and()
+        .logout()
+        .logoutSuccessUrl("/logout-success");
+  }
+}
