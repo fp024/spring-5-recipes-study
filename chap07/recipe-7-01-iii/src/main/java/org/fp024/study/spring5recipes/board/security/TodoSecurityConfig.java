9c34b402d6a696daa62e3efff5abe499a1e1a265
fp024 <aopenasus@naver.com>
2023-11-27 00:13:31 +0900

    MvcRequestMatcher 사용하도록 변경
    * #14
    * `spring-security/issues/13602#issuecomment-1816499124`
      * 위에 가이드 나온데로 설정하면 MvcRequestMatcher를 사용하면서 동작에 문제는 없음.

--- chap07/recipe-7-01-iii/src/main/java/org/fp024/study/spring5recipes/board/security/TodoSecurityConfig.java
+++ chap07/recipe-7-01-iii/src/main/java/org/fp024/study/spring5recipes/board/security/TodoSecurityConfig.java
@@ -1,7 +1,6 @@
 package org.fp024.study.spring5recipes.board.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import lombok.val;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +12,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 /*
   WebSecurityConfigurerAdapter가 지원 중단 되었고,
@@ -44,7 +45,20 @@ public class TodoSecurityConfig {
   }
 
   @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+    return new HandlerMappingIntrospector();
+  }
+
+  @Bean
+  MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+    return new MvcRequestMatcher.Builder(introspector);
+  }
+
+  // https://github.com/spring-projects/spring-security/issues/13602#issuecomment-1816499124
+  // 내용대로 설정을 변경, 이러면 확실히 `MvcRequestMatcher`를 사용하는 상태가 되긴함.
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc)
+      throws Exception {
     http.authorizeHttpRequests(
         (authz) ->
             authz
@@ -57,13 +71,13 @@ public class TodoSecurityConfig {
                     "/favicon.ico")
                  */
                 .requestMatchers(
-                    antMatcher("/webjars/**"), //
-                    antMatcher("/resources/**"),
-                    antMatcher("/"),
-                    antMatcher("/index"),
-                    antMatcher("/favicon.ico"))
+                    mvc.pattern("/webjars/**"), //
+                    mvc.pattern("/resources/**"),
+                    mvc.pattern("/"),
+                    mvc.pattern("/index"),
+                    mvc.pattern("/favicon.ico"))
                 .permitAll()
-                .requestMatchers(antMatcher(HttpMethod.DELETE, "/todos/*"))
+                .requestMatchers(mvc.pattern(HttpMethod.DELETE, "/todos/*"))
                 .hasAuthority("ADMIN")
                 .anyRequest()
                 .authenticated());
