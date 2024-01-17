7d00bac16dec7a09f54c3190cb2638aed3954c39
fp024 <aopenasus@naver.com>
2024-01-18 03:18:24 +0900

    레시피 9-01-i JDBC 템플릿으로 DB 수정하기 - insert
    `PreparedStatementCreator`를 활용해서 INSERT 수행

---
+++ chap09/recipe-9-01-i/src/main/java/org/fp024/study/spring5recipes/vehicle/config/VehicleConfiguration.java
@@ -0,0 +1,69 @@
+package org.fp024.study.spring5recipes.vehicle.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.nio.charset.StandardCharsets;
+import javax.sql.DataSource;
+import org.fp024.study.spring5recipes.vehicle.dao.JdbcVehicleDao;
+import org.fp024.study.spring5recipes.vehicle.dao.VehicleDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+@Configuration
+@ComponentScan("org.fp024.study.spring5recipes.vehicle")
+@PropertySource("classpath:database-mysql.properties")
+public class VehicleConfiguration {
+
+  private final Environment env;
+
+  public VehicleConfiguration(Environment env) {
+    this.env = env;
+  }
+
+  @Bean
+  VehicleDao vehicleDao(DataSource dataSource) {
+    return new JdbcVehicleDao(dataSource, jdbcTemplate(dataSource));
+  }
+
+  @Bean
+  JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
+
+  @Bean(destroyMethod = "close")
+  HikariDataSource dataSource() {
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setDriverClassName(env.getProperty("jdbc.driver"));
+    hikariConfig.setJdbcUrl(env.getProperty("jdbc.url"));
+    hikariConfig.setUsername(env.getProperty("jdbc.username"));
+    hikariConfig.setPassword(env.getProperty("jdbc.password"));
+    hikariConfig.setMinimumIdle(2);
+    hikariConfig.setMaximumPoolSize(5);
+    return new HikariDataSource(hikariConfig);
+  }
+
+  @Bean
+  DataSourceInitializer dataSourceInitializer() {
+    DataSourceInitializer initializer = new DataSourceInitializer();
+    initializer.setDataSource(dataSource());
+    initializer.setDatabasePopulator(databasePopulator());
+    return initializer;
+  }
+
+  @Bean
+  ResourceDatabasePopulator databasePopulator() {
+    ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+    databasePopulator.setSqlScriptEncoding(StandardCharsets.UTF_8.name());
+    databasePopulator.setContinueOnError(false);
+    databasePopulator.addScript(new ClassPathResource("sql/mysql/schema.sql"));
+
+    return databasePopulator;
+  }
+}
---
+++ chap09/recipe-9-01-i/src/test/java/org/fp024/study/spring5recipes/vehicle/dao/JdbcVehicleDaoTests.java
@@ -0,0 +1,35 @@
+package org.fp024.study.spring5recipes.vehicle.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fp024.study.spring5recipes.vehicle.config.VehicleConfiguration;
+import org.fp024.study.spring5recipes.vehicle.domain.Vehicle;
+import org.fp024.study.spring5recipes.vehicle.test.DBTestUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@Slf4j
+@SpringJUnitConfig(classes = VehicleConfiguration.class)
+class JdbcVehicleDaoTests {
+
+  @Autowired private VehicleDao vehicleDao;
+
+  @Autowired private DBTestUtils dbTestUtils;
+
+  @AfterEach
+  void AfterEach() {
+    dbTestUtils.resetDB();
+  }
+
+  @Test
+  void insert() {
+    Vehicle v1 = new Vehicle("TEM0001", "Red", 4, 4);
+    vehicleDao.insert(v1);
+
+    assertThat(vehicleDao.findByVehicleNo(v1.getVehicleNo())) //
+        .hasFieldOrPropertyWithValue("vehicleNo", v1.getVehicleNo());
+  }
+}
