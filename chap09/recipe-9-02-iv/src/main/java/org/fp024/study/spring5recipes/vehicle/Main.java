7bc1b2d4a8ea60104bca78ffdf3d80eb9cdea2a9
fp024 <aopenasus@naver.com>
2024-01-21 01:15:41 +0900

    레시피 9-02-v - Skip
    - v는 이미 다해본 내용이여서 따로 프로젝트를 만들지 않음.
    
    - iv 코드 정리

--- chap09/recipe-9-02-iv/src/main/java/org/fp024/study/spring5recipes/vehicle/Main.java
+++ chap09/recipe-9-02-iv/src/main/java/org/fp024/study/spring5recipes/vehicle/Main.java
@@ -1,10 +1,8 @@
 package org.fp024.study.spring5recipes.vehicle;
 
 import java.util.Arrays;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.fp024.study.spring5recipes.vehicle.dao.VehicleDao;
-import org.fp024.study.spring5recipes.vehicle.domain.Vehicle;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -21,16 +19,15 @@ public class Main {
 
   void run(String[] args) {
     LOGGER.info("commonad args:", Arrays.toString(args));
-
-    List<Vehicle> vehicles =
-        List.of(
-            new Vehicle("TEM0022", "Blue", 4, 4),
-            new Vehicle("TEM0023", "Black", 4, 6),
-            new Vehicle("TEM0024", "Green", 4, 5));
-
-    vehicleDao.insert(vehicles);
-
-    vehicleDao.findAll().forEach(System.out::println);
+    vehicleDao
+        .findAll()
+        .forEach(
+            vehicle -> {
+              System.out.printf("Vehicle No: %s%n", vehicle.getVehicleNo());
+              System.out.printf("Color: %s%n", vehicle.getColor());
+              System.out.printf("Wheel: %d%n", vehicle.getWheel());
+              System.out.printf("Seat: %d%n", vehicle.getSeat());
+            });
   }
 
   public static void main(String[] args) {
