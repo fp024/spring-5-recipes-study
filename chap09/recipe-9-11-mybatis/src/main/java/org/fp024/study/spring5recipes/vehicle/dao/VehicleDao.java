b4d2365ea987f8e0b9f24ae41f4f6e0bc84db387
fp024 <aopenasus@naver.com>
2024-01-24 21:18:03 +0900

    매퍼 인터페이스에 있는 메서드는 인터페이스 상속으로 중복 선언을 제거

--- chap09/recipe-9-11-mybatis/src/main/java/org/fp024/study/spring5recipes/vehicle/dao/VehicleDao.java
+++ chap09/recipe-9-11-mybatis/src/main/java/org/fp024/study/spring5recipes/vehicle/dao/VehicleDao.java
@@ -2,23 +2,8 @@ package org.fp024.study.spring5recipes.vehicle.dao;
 
 import java.util.List;
 import org.fp024.study.spring5recipes.vehicle.domain.Vehicle;
+import org.fp024.study.spring5recipes.vehicle.mapper.VehicleMapper;
 
-public interface VehicleDao {
-  void insert(Vehicle vehicle);
-
+public interface VehicleDao extends VehicleMapper {
   void insert(List<Vehicle> vehicles);
-
-  void insertVehicles(List<Vehicle> vehicles);
-
-  void update(Vehicle vehicle);
-
-  void delete(Vehicle vehicle);
-
-  Vehicle findByVehicleNo(String vehicleNo);
-
-  List<Vehicle> findAll();
-
-  String getColor(String vehicleNo);
-
-  Integer countAll();
 }
