bd9ba37da25a861e6d4158652b69ed2ae701ea8f
fp024 <aopenasus@naver.com>
2024-04-06 11:04:59 +0900

    [#23] TOML 파일로 버전관리 - 3장 수정

--- chap03/recipe-3-12/src/main/java/org/fp024/study/spring5recipes/court/domain/Reservation.java
+++ chap03/recipe-3-12/src/main/java/org/fp024/study/spring5recipes/court/domain/Reservation.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
